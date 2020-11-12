44cceba060c2922ef6973ae8fe9d6751e48163ad
DougKeller <dkeller@topechelon.com>
2020-11-12 12:46:32 -0500

    Define schema for component options

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,9 +1,9 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { BBComponentSchematics } from './schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function component(_options: any): Rule {
+export function component(_options: BBComponentSchematics): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
