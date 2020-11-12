f7ee9591c43b9b8d6c17804fd2ec8204bfc4faf2
DougKeller <dkeller@topechelon.com>
2020-11-12 13:21:08 -0500

    Add template for component ts file

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,10 +1,53 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  apply,
+  mergeWith,
+  template,
+  url
+} from '@angular-devkit/schematics';
 import { BBComponentSchematics } from './schema';
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function component(_options: BBComponentSchematics): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+import { strings } from '@angular-devkit/core';
+
+const IGNORED_PREFIXES = [
+  'shared',
+  'feature'
+];
+
+function getPathParts(path: string): Array<string> {
+  let parts: Array<string> = path.split('/');
+  parts = parts.map(strings.dasherize);
+  return parts.filter(part => !IGNORED_PREFIXES.includes(part));
+}
+
+function buildClassName(path: string): string {
+  return strings.classify(getPathParts(path).join('-'));
+}
+
+function buildSelector(path: string): string {
+  return 'bb-' + getPathParts(path).join('-');
+}
+
+function buildFileName(path: string): string {
+  let parts: Array<string> = getPathParts(path);
+  return parts[parts.length - 1];
+}
+
+export function component(options: BBComponentSchematics): Rule {
+  return (_tree: Tree, _context: SchematicContext) => {
+    const { path } = options;
+
+    const sourceTemplates = url('./files');
+    const sourceParameterizedTemplates = apply(sourceTemplates, [
+      template({
+        selector: buildSelector(path),
+        fileName: buildFileName(path),
+        buildClassName: buildClassName(path)
+      })
+    ]);
+
+    return mergeWith(sourceParameterizedTemplates);
   };
 }
--- src/component/schema.d.ts
+++ src/component/schema.d.ts
@@ -1,3 +1,3 @@
 export interface BBComponentSchematics {
-  name: string;
+  path: string;
 }
