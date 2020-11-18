18bc721ab6c34d82e47ef1cd4c72430e353932b1
DougKeller <dkeller@topechelon.com>
2020-11-18 10:35:06 -0500

    Add service generator

---
+++ src/service/index.ts
@@ -0,0 +1,21 @@
+import { Rule, SchematicContext, Tree, url } from '@angular-devkit/schematics';
+import { BBServiceSchematics } from './schema';
+
+import { generateFiles } from '../utility/generators';
+import { buildFileName, buildDirectory, buildClassName } from '../utility/naming';
+
+function buildConfig(options: BBServiceSchematics) {
+  return {
+    fileName: buildFileName(options.path),
+    className: buildClassName(options.path, 'Service'),
+    directory: buildDirectory(options.path)
+  };
+}
+
+export function service(options: BBServiceSchematics): Rule {
+  return (_tree: Tree, _context: SchematicContext) => {
+    let config = buildConfig(options);
+
+    return generateFiles(url('./files'), config);
+  };
+}
